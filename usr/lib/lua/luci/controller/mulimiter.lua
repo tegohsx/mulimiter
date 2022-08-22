7348683a36373cd1516e0e51c3880450b0954ec8
Teguh Santoso <101353193+tegohsx@users.noreply.github.com>
2022-08-22 09:40:05 +0700

    Update mulimiter.lua

--- usr/lib/lua/luci/controller/mulimiter.lua
+++ usr/lib/lua/luci/controller/mulimiter.lua
@@ -1,4 +1,4 @@
 module("luci.controller.mulimiter", package.seeall)
 function index()
-entry({"admin","services","mulimiter"}, template("mulimiter"), _("MuLimiter"), 55).leaf=true
+entry({"admin","services","mulimiter"}, template("mulimiter"), _("MulImiter"), 55).leaf=true
 end
